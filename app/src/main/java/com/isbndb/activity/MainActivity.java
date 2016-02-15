e4e7a9a6a135145d5b503f63174dedc2a667f31f
sgsudhir <sgsudhir@hotmail.com>
2016-02-15 23:32:12 +0530

    Initialize repository

---
+++ app/src/main/java/com/isbndb/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.isbndb.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.isbndb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
